06601e115f6d595fdfc2b1659e0a29de3ae46190
Rasmus Pedersen <rasmuspeders1@gmail.com>
2017-07-12 15:39:27 +0200

    Added esp-request as submodule and using it to post data to influxdb

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,26 @@
 #include <string>
 #include <cstring>
 #include <cstdint>
+#include <cstdio>
 #include "main.hpp"
 #include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_event_loop.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 
 #include "Arduino.h"
 #include "DHT.h"
+extern "C" {
+    #include "esp_request.h"
+}
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+static const char *TAG = "REQAPP";
 
 //set with make menuconfig
 #define DEFAULT_SSID CONFIG_WIFI_SSID
@@ -22,18 +32,34 @@ DHT dht(DHTPIN, DHTTYPE);
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
     return ESP_OK;
 }
 
-extern "C" void app_main(void)
+
+static void initialise_wifi(void)
 {
-    nvs_flash_init();
     tcpip_adapter_init();
+    wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     wifi_sta_config_t sta_config;
     uint8_t ssid[32] = DEFAULT_SSID;
     memcpy(sta_config.ssid, ssid, sizeof(ssid));
@@ -42,9 +68,40 @@ extern "C" void app_main(void)
     sta_config.bssid_set = false;
     wifi_config_t wifi_config;
     wifi_config.sta = sta_config;
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+    
+}
+
+void http_request_task(void *data)
+{
+    request_t *req;
+    int status;
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to AP, freemem=%d",esp_get_free_heap_size());
+    // vTaskDelay(1000/portTICK_RATE_MS);
+    req = req_new("http://rpi.odin:8086/write?db=temptest");
+    char post[] = "POST";
+    req_setopt(req, REQ_SET_METHOD, post);
+    req_setopt(req, REQ_SET_POSTFIELDS, data);
+    status = req_perform(req);
+    req_clean(req);
+    ESP_LOGI(TAG, "Finish request, status=%d, freemem=%d", status, esp_get_free_heap_size());
+    vTaskDelete(NULL);
+}
+
+extern "C" void app_main(void)
+{
+    nvs_flash_init();
+    initialise_wifi();
+    request_t *req;
+    int status;
+    xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, 30000 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Connected to AP, freemem=%d",esp_get_free_heap_size());
+
+    char data[1024];
 
     dht.begin();
     while (true) {
@@ -57,7 +114,11 @@ extern "C" void app_main(void)
             printf("Temperature %g °C\n", temperature);
             printf("humidity %g %%\n", humidity);
         }
+        sprintf(data, "temperature value=%f\nhumidity value=%f",temperature, humidity);
+        xTaskCreate(&http_request_task, "request_task", 8192, data, 5, NULL);
+
         vTaskDelay(3000 / portTICK_PERIOD_MS);
     }
+    
 }
 
